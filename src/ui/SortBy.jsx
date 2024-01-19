dcc8b532ca4009eee36549d1989c1d8405e22d8c
babacar <babacar.diakite01@gmail.com>
2024-01-18 19:56:05 -0500

    filter

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,6 +1,28 @@
 
 import styled from "styled-components";
+const FilterButton = styled.button`
+  background-color: var(--color-grey-0);
+  border: none;
 
+  ${(props) =>
+    props.active &&
+    css`
+      background-color: var(--color-brand-600);
+      color: var(--color-brand-50);
+    `}
+
+  border-radius: var(--border-radius-sm);
+  font-weight: 500;
+  font-size: 1.4rem;
+  /* To give the same height as select */
+  padding: 0.44rem 0.8rem;
+  transition: all 0.3s;
+
+  &:hover:not(:disabled) {
+    background-color: var(--color-brand-600);
+    color: var(--color-brand-50);
+  }
+`;
 const StyledSelect = styled.select`
   font-size: 1.4rem;
   padding: 0.8rem 1.2rem;
--- src/ui/SortOperation.jsx
+++ src/ui/SortOperation.jsx
@@ -5,8 +5,7 @@ export default function RecipeOperation() {
 
 
     return    <SortBy options={[
-     { value: "name-a-z", label: "Sort by name (A-Z)" },
-     { value: "name-z-a", label: "sort by name (Z-A)" },
+
      {value: "breakfast", label: "breakfast"},
      {value: "lunch", label: "lunch"},
      {value: "supper", label: "supper"},
