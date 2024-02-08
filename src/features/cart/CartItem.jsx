fca8fd770bf52eb4837105888c776756226efe83
babacar <babacar.diakite01@gmail.com>
2024-02-07 21:13:18 -0500

    test2

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -10,7 +10,7 @@ export default function CartItem({ item }) {
        {List?.map((ele, index) => <div className=" block w-full  rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(247, 247, 247, 0.07),0_10px_20px_-2px_rgba(223, 201, 201, 0.04)] dark:bg-neutral-700">
   <ul className="w-full  ">
     <li
-      className="w-full border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50 text-white">
+      className="w-full border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50 text-red-700">
      {index +1}-{ele}
     </li>
    
